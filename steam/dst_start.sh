7d7aa59f0029875694686bb926c8cf47ef4f8ff8
Juan Angel <Kaekh@users.noreply.github.com>
2022-11-27 16:39:20 +0100

    Update dst_start.sh

--- steam/dst_start.sh
+++ steam/dst_start.sh
@@ -12,14 +12,16 @@ fi
 
 cd "$steam_dir"
 
-bash steamcmd +force_install_dir "$install_dir" +login anonymous +app_update 343050 validate +quit
+bash steamcmd +force_install_dir "$install_dir" +login anonymous +app_update 343050 +quit
 
 
 if [[ -d "$install_dir/bin64" && -e "$steam_dir/files/dedicated_server_mods_setup.lua" ]]; then
         cp "$steam_dir/files/dedicated_server_mods_setup.lua" "$install_dir/mods/"
 fi
 
-run_shared=("$install_dir/bin64/dontstarve_dedicated_server_nullrenderer_x64")
+cd "$install_dir/bin64"
+
+run_shared=("./dontstarve_dedicated_server_nullrenderer_x64")
 run_shared+=(-console)
 run_shared+=(-cluster "${SERVERNAME}")
 run_shared+=(-monitor_parent_process $$)
